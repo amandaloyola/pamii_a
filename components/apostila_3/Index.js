b4978e2a66033739632278991e968f8b2884262e
amandaloyola <124743646+amandaloyola@users.noreply.github.com>
2023-05-09 20:50:36 -0300

    aula 9/05

--- components/apostila_3/Index.js
+++ components/apostila_3/Index.js
@@ -1,11 +1,20 @@
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 
 import styles from './styles'
 
 export default function apostila03() {
   return (
     <View style={styles.container}>
-      <Text>Exemplo apostila 3</Text>
+      <Text style={styles.paragraph}>Exemplo apostila 3</Text>
+
+      <TouchableOpacity
+       style={styles.button}
+       onPress={()=>alert('oi')}
+       >
+
+      <Text style= {styles.textButton}>HELLO WORLD!!!</Text>
+      </TouchableOpacity>
+
     </View>
   );
 }
