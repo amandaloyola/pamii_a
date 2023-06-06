66cc64a85011fb55bbc6192d29a79479218515c9
amandaloyola <124743646+amandaloyola@users.noreply.github.com>
2023-06-06 20:11:30 -0300

    aulaa

--- components/apostila_4/index.js
+++ components/apostila_4/index.js
@@ -1,13 +1,13 @@
-import { View, Text } from 'react-native';
+import React, { useState } from 'react'; 
+import { View, Text } from 'react-native'; 
 
-
-import styles from'./styles';
+import styles from './styles';
 
 function Apostila04() {
-    return (
-       <View style={styles.container}> 
-            <Text style={styles.texto}> Exemplo 4</Text>
-       </View> 
+    return(
+        <View style={styles.container}>
+            <Text>Exemplo 4</Text>
+        </View>
     );
 }
 
