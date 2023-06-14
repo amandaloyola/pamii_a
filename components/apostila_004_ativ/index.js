4de18217dac210c943bd4dc86423fd106853c055
amandaloyola <124743646+amandaloyola@users.noreply.github.com>
2023-06-13 22:18:21 -0300

    aula13/06-2

--- components/apostila_004_ativ/index.js
+++ components/apostila_004_ativ/index.js
@@ -7,38 +7,39 @@ function Apostila_004_ativ() {
   const [entrada1, setEntrada1] = useState('');
   const [mensagem, setMensagem] = useState('Inserir o nome e sobrenome');
 
-    function apresentaMensagem() {
-        setMensagem(entrada+''+Entrada1);
-        setEntrada('');
-        setEntrada1('');
+function apresentaMensagem() {
+  setMensagem(entrada+''+entrada1);
+  setEntrada('');
+  setEntrada1('');
 }
 
-  return ( 
-    <View style={styles.container}>
-      <Text style={styles.texto}>Atividade Apostila 4</Text>
+return ( 
+  <View style={styles.container}>
+  <Text style={styles.texto}>Atividade Apostila 4</Text>
 
-      <Text style={styles.txtSaida}>{mensagem}</Text>
-      <TextInput style={styles.txtEntrada}
-            onChangeText={ (inputEntrada) => setEntrada(inputEntrada)}
-            value={entrada}
-      />
+  <Text style={styles.txtSaida}>{mensagem}</Text>
+  <TextInput style={styles.txtEntrada}
+    onChangeText={ (inputEntrada) => setEntrada(inputEntrada)}
+    value={entrada}
+  />
 
-<Text style={styles.txtSaida}>{mensagem}</Text>
-      <TextInput style={styles.txtEntrada}
-            onChangeText={ (inputEntrada) => setEntrada1(inputEntrada)}
-            value={entrada1}
-      />
+  <Text style={styles.txtSaida}>{mensagem}</Text>
+  <TextInput style={styles.txtEntrada}
+    onChangeText={ (inputEntrada) => setEntrada1(inputEntrada)}
+    value={entrada1}
+/>
 
-      <TouchableOpacity 
-        style={styles.button} 
-        onPress={() => apresentaMensagem()}
-      >
-            <Text style={styles.textButton}>
-              Exibir Texto
-            </Text>
-      </TouchableOpacity>
+  <TouchableOpacity 
+    style={styles.button} 
+    onPress={() => apresentaMensagem()}
+    >
+  <Text style={styles.textButton}>
+    Exibir Texto
+  </Text>
 
-    </View>
+  </TouchableOpacity>
+
+  </View>
   );
 }
 export default Apostila_004_ativ;
