73b3399c04f691b61b36e124cd6f12005f6ea1ca
amandaloyola <124743646+amandaloyola@users.noreply.github.com>
2023-06-22 20:51:24 -0300

    aulaa22/06

--- App.js
+++ App.js
@@ -3,17 +3,18 @@ import { StyleSheet, View } from 'react-native';
 
 import Constants from 'expo-constants'; 
 
-//import Apostila_3 from './components/apostila_3';
-//import Apostila_4 from './components/apostila_4';
-//import Apostila_004_ativ from './components/apostila_004_ativ';
-import Index from './components/apostila_5';
+import Apostila_3 from './components/apostila_3';
+import Apostila_4 from './components/apostila_4';
+import Apostila_004_ativ from './components/apostila_004_ativ';
+import Apostila_05 from './components/apostila_5';
+import apostila_6 from './components/apostila_6'
 
 let corApp = '#000';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <apostila_5 />
+      <apostila_6 />
       <StatusBar style={corApp==='#000' ? 'light' : 'dark'}/>
     </View>
   );
@@ -22,8 +23,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center', 
-    alignItems: 'center', 
+    //justifyContent: 'center', 
+    //alignItems: 'center', 
     backgroundColor: corApp,
     padding: 8, 
     paddingTop: Constants.statusBarHeight,
